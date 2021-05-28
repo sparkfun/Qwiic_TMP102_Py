bcac7406634f07c194ca90fb8d8980b60cae2271
santaimpersonator <36016723+santaimpersonator@users.noreply.github.com>
2021-05-28 05:09:45 -0600

    Add copyright
    Add copyright comment block to match first example

--- examples/Example2_One-Shot_Temperature_Reading.py
+++ examples/Example2_One-Shot_Temperature_Reading.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 #-----------------------------------------------------------------------------
 # Example2_One-Shot_Temperature_Reading.py
+# 
 # Example for the TMP102 I2C Temperature Sensor
-# Alex Wende @ SparkFun Electronics
+# Written byAlex Wende @ SparkFun Electronics
 # April 29th 2016
-
+# 
 # This sketch connects to the TMP102 temperature sensor and enables the
 # one-shot temperature measurement mode using the one_shot() function.
 # The function returns 0 until the temperature measurement is ready to
@@ -12,11 +13,29 @@
 # placed back into sleep mode before the loop is repeated. This can be 
 # useful to save power or increase the continuous conversion rate from
 # 8Hz up to a maximum of 40Hz.
-
-# This code is beerware; if you see me (or any other SparkFun employee) at
-# the local, and you've found our code helpful, please buy us a round!
-
-# Distributed as-is; no warranty is given.   
+# 
+#==================================================================================
+# Copyright (c) 2021 SparkFun Electronics
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy 
+# of this software and associated documentation files (the "Software"), to deal 
+# in the Software without restriction, including without limitation the rights 
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+# copies of the Software, and to permit persons to whom the Software is 
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all 
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+# SOFTWARE.
+#==================================================================================
+# Example 2
 #-----------------------------------------------------------------------------
 
 from __future__ import print_function
